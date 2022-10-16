fad54a42e0cfac8068159309c4d554a94d0e882a
Apolloscar <ohernandez4869@gmail.com>
2022-10-15 21:41:50 -0700

    finished branch

--- hw2-handout/branch/branch.go
+++ hw2-handout/branch/branch.go
@@ -9,7 +9,28 @@ import (
 func branchCount(fn *ast.FuncDecl) uint {
 	// TODO: Write the branchCount function,
 	// count the number of branching statements in function fn
-	return 12345
+
+	count := 0
+	ast.Inspect(fn, func(node ast.Node) bool { // look for any of the branch statements and if found, add to count
+		switch node.(type) {
+		case *ast.IfStmt:
+			count += 1
+		case *ast.ForStmt:
+			count += 1
+		case *ast.SwitchStmt:
+			count += 1
+		case *ast.TypeSwitchStmt:
+			count += 1
+		case *ast.RangeStmt:
+			count += 1
+		}
+
+		// If we return true, we keep recursing under this AST node.
+		// If we return false, we won't visit anything under this AST node.
+		return true
+	})
+
+	return uint(count)
 }
 
 // ComputeBranchFactors returns a map from the name of the function in the given
